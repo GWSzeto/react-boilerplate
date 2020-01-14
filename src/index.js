89e35037184686cfbb226c7334acffd7e079c553
Stanley Szeto <sszeto@uwaterloo.ca>
2020-01-14 15:32:27 -0500

    added css reset

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Reset } from 'styled-reset'
 
 const App = () => (
-  <div>Ayooo</div>
+  <div>
+    <Reset/>
+    asd
+  </div>
 )
 
 ReactDOM.render(<App/>, document.getElementById("root"))
